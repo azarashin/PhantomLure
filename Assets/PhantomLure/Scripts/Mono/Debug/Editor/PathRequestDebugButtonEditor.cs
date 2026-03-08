ff44a0fb7d3f4432bec90d486e4c2c428fd8bafa
azarashin <azarashin1979@gmail.com>
2026-03-09 07:19:51 +0900

    パスを求めるリクエストを送るデバッグ用機能を追加。

---
+++ Assets/PhantomLure/Scripts/Mono/Debug/Editor/PathRequestDebugButtonEditor.cs
@@ -0,0 +1,33 @@
+﻿using PhantomLure.Debugging;
+using UnityEditor;
+using UnityEngine;
+
+namespace PhantomLure.Debugging.NSEditor
+{
+    [CustomEditor(typeof(PathRequestDebugButton))]
+    public class PathRequestDebugButtonEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.Space();
+
+            var button = (PathRequestDebugButton)target;
+
+            GUI.enabled = Application.isPlaying;
+            if (GUILayout.Button("Send Path Request"))
+            {
+                button.SendPathRequest();
+            }
+            GUI.enabled = true;
+
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox(
+                    "Play中にボタンを押してください。",
+                    MessageType.Info);
+            }
+        }
+    }
+}
